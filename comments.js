aa8394e6cf0e2de65129055d09bdafe2622b811d
JunaidWali <56029841+JunaidWali@users.noreply.github.com>
2023-11-08 03:43:30 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,30 +1,63 @@
 // create web server
-// 1. load modules
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-// 2. create web server object
-var server = http.createServer(function(request, response){
-    // 2.1 parse url
-    var parsedUrl = url.parse(request.url);
-    // 2.2 get path
-    var resource = parsedUrl.pathname;
-    console.log(resource);
-    // 2.3 get query string as object
-    var query = qs.parse(parsedUrl.query);
-    console.log(query);
-    // 2.4 get method
-    var method = request.method;
-    console.log(method);
-    // 2.5 set response header
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    // 2.6 send response
-    response.end('Hello World');
+// load modules
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// create web server
+var app = express();
+
+// configure body-parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// connect to database
+mongoose.connect('mongodb://localhost:27017/mean', function(err) {
+  if (err) {
+    console.log('Failed connecting to MongoDB!');
+  } else {
+    console.log('Successfully connected to MongoDB!');
+  }
 });
-// 3. start server
-server.listen(8080, function(){
-    console.log('Server is running...');
+
+// define schema
+var CommentSchema = new mongoose.Schema({
+  name: String,
+  comment: String
+});
+
+// define model
+var Comment = mongoose.model('Comment', CommentSchema);
+
+// define routes
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/comments', function(req, res) {
+  Comment.find({}, function(err, docs) {
+    res.json(docs);
+  });
+});
+
+app.post('/comments', function(req, res) {
+  console.log(req.body);
+  var comment = new Comment(req.body);
+  comment.save(function(err, docs) {
+    res.json(docs);
+  });
+});
+
+app.delete('/comments/:id', function(req, res) {
+  console.log(req.params.id);
+  Comment.remove({_id: req.params.id}, function(err, docs) {
+    res.json(docs);
+  });
+});
+
+// start web server
+app.listen(3000, function() {
+  console.log('Server running at http://localhost:3000');
 });
 
 
